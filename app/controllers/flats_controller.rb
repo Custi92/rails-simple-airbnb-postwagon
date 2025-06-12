c2fae4419b9b5dba4896c1b831858fdd73dd69a8
Marcus Meyer <marcusmeyer90@hotmail.fr>
2025-06-12 11:52:25 +0200

    Ajout de create + bouton home

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,9 +2,25 @@ class FlatsController < ApplicationController
 
   def index
     @flats = Flat.all
+    @flat = Flat.new
   end
 
   def show
     @flat = Flat.find(params[:id])
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
