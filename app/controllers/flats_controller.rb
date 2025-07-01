3905443b22b721d5e52e129978d17cd60ab6cf25
Marcus Meyer <marcusmeyer90@hotmail.fr>
2025-07-01 17:02:39 +0200

    Create et Front

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,11 +2,14 @@ class FlatsController < ApplicationController
 
   def index
     @flats = Flat.all
-    @flat = Flat.new
   end
 
   def show
     @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
   end
 
   def create
@@ -21,6 +24,6 @@ class FlatsController < ApplicationController
   private
 
   def flat_params
-    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :photo)
   end
 end
