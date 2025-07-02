f144b52a5f77e12cfe4934ce6c443f2349fbc099
Marcus Meyer <marcusmeyer90@hotmail.fr>
2025-07-02 11:35:20 +0200

    Action edit, update, destroy et front

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,6 +2,7 @@ class FlatsController < ApplicationController
 
   def index
     @flats = Flat.all
+    @flat = Flat.new
   end
 
   def show
@@ -19,6 +20,25 @@ class FlatsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
   end
 
   private
